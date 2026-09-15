44d6219d5c78f98588b0bfcae4f974c6717a466b
RajAnand-2025 <rajanand23023@gmail.com>
2026-09-16 01:22:26 +0530

    remove goole auth

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -373,3 +373,5 @@ drop policy if exists medical_reports_storage_insert on storage.objects;
 create policy medical_reports_storage_insert on storage.objects for insert with check (bucket_id = 'medical-reports' and (storage.foldername(name))[1] = auth.uid()::text);
 drop policy if exists medical_reports_storage_delete on storage.objects;
 create policy medical_reports_storage_delete on storage.objects for delete using (bucket_id = 'medical-reports' and (storage.foldername(name))[1] = auth.uid()::text);
+
+notify pgrst, 'reload schema';
